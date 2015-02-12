1f8e623e02faffcb1148616028f55c1783aad7c1
hernaez <d@fierostudios.com>
2015-02-12 16:24:15 -0300

    Updated UI with linked buttons to scenes

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour {
+
+	[SerializeField]
+	List<GameObject> screenList;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnMenuButtonPressed(string menu)
+	{
+
+		foreach ( GameObject screen in screenList)
+		{
+			if (screen.name != menu)
+				screen.SetActive(false);
+			else
+				screen.SetActive(true);
+		}
+
+
+	}
+}
